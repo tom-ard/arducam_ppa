b04ad999a12fd9ffd7ca5368feafa0bbcb87e0ac
tom-ard <tom@arducam.com>
2022-11-08 13:48:43 +0800

    Add libcamera-dev and libcamera-apps, version: 0.0.10.

--- update.sh
+++ update.sh
@@ -4,5 +4,9 @@ debs=$(ls $debs_path)
 
 for deb in $debs; do
     echo $deb
-    reprepro --basedir $(pwd) includedeb linux $debs_path/$deb
+    if [[ "bullseye" == *$deb* ]];then
+        reprepro --basedir $(pwd) includedeb bullseye $debs_path/$deb
+    else
+        reprepro --basedir $(pwd) includedeb linux $debs_path/$deb
+    fi
 done
